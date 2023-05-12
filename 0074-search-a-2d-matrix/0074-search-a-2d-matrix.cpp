46fe4382722f82243243eef692aa0b036d66382b
Abhishek Kumar <kumarabhishek0776@gmail.com>
2023-05-12 10:34:54 +0530

    Time: 10 ms (12.65%), Space: 9.4 MB (96.21%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        
+        int begin=0, end=n*m-1;
+        
+        // Applying binarySearch
+        // Here we are treating 2D matrix as sorted list. To convert 2D matrix in single array we are         // using  mid/m which gives row index and mid%m which gives column index
+        
+        
+        while(begin<=end){
+            
+            int mid=begin+(end-begin)/2;
+            
+            if(matrix[mid/m][mid%m]==target)return true;
+            else if(matrix[mid/m][mid%m]<target)begin=begin+1;
+            else end=end-1;
+        }
+        
+        return false;
+    }
+};
+// TC=O(log(M*N)), SC=O(1);
