db492a6d656139a4316d5b38149e92392a0bbd22
Abhishek Kumar <kumarabhishek0776@gmail.com>
2023-05-27 11:34:28 +0530

    Time: 63 ms (61.68%), Space: 24.4 MB (62.15%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -18,8 +18,23 @@ public:
         int n=text1.size();
         int m=text2.size();
         
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return f(n-1, m-1, text1, text2, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        //return f(n-1, m-1, text1, text2, dp);
+        // Tabulation
+        
+        
+         for(int i=1; i<=n; i++){
+             for(int j=1; j<=m; j++){
+                 
+                 if(text1[i-1]==text2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                 else dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+             }
+         }
+        
+        return dp[n][m];
+        
             
     }
 };
+
+// TC=O(N*M), SC=O(N*M)
