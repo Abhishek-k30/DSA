1cb58e9e8776e8b3f94c1b2c1c43932f99354b4f
Abhishek Kumar <kumarabhishek0776@gmail.com>
2023-05-30 11:00:03 +0530

    Time: 4 ms (38.89%), Space: 6.2 MB (12.64%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -1,13 +1,39 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        vector<int> factors;
-        for(int i=1;i<=n; i++){
-            if(n%i==0)factors.push_back(i);
+//         vector<int> factors;
+//         for(int i=1;i<=n; i++){
+//             if(n%i==0)factors.push_back(i);
+//         }
+        
+//         if(factors.size()<k)return -1;
+//         return factors[k-1];
+        //TC=O(N), SC=O(N);
+        
+        vector<int> frontFac, backFac;
+        // Run a loop till sqrt(n);
+        frontFac.push_back(1);
+        for(int i=2; i*i<=n; i++){
+            
+            if(n%i==0){
+                
+            frontFac.push_back(i);
+            
+            if(i*i!=n){ // to prevent duplicate values;
+                
+                backFac.push_back(n/i);
+                
+            }}
         }
         
-        if(factors.size()<k)return -1;
-        return factors[k-1];
+        for(int i=backFac.size()-1;i>=0; i--){
+            frontFac.push_back(backFac[i]);
+        }
+        frontFac.push_back(n);
+        
+        if(frontFac.size()<k)return -1;
+        return frontFac[k-1];
+        
         
         
     }
