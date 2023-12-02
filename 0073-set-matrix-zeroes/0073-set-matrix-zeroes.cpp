5b431c3c380ae76b07024290ebd75b55d4a48d4e
Abhishek Kumar <kumarabhishek0776@gmail.com>
2023-12-02 08:39:59 +0530

    Time: 12 ms (56.49%), Space: 13.9 MB (49.41%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,43 +1,48 @@
 class Solution {
 public:
-    void setMatrix(vector<vector<int>>& matrix , int i , int j){
-        int n=matrix.size();
-        int m= matrix[0].size();       
-        int ind1=i , ind2=j;
-                   while(ind1>=0){
-                       matrix[ind1][ind2]=0;
-                       ind1--;
-                   }
-                   ind1=i , ind2=j;
-                   while(ind1<n){
-                       matrix[ind1][ind2]=0;
-                       ind1++;
-                   }
-                    ind1=i , ind2=j;
-                   while(ind2>=0){
-                       matrix[ind1][ind2]=0;
-                       ind2--;
-                   }
-                   ind1=i , ind2=j;
-                   while(ind2<m){
-                       matrix[ind1][ind2]=0;
-                       ind2++;
-                   }
-                   ind1=i , ind2=j;
-    }
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        int m= matrix[0].size();
-        vector<pair<int , int>> vp;
-       for(int i=0 ; i<matrix.size() ; i++){
-           for(int j=0 ; j<matrix[i].size() ; j++){
-               if(matrix[i][j]==0){
-                 vp.push_back({i , j});
-               }
-           }
-       } 
-        for(int i=0 ; i<vp.size() ; i++){
-            setMatrix(matrix ,vp[i].first , vp[i].second);
+        int m=matrix[0].size();
+        
+        // flags for tracking zero of first row and column
+        bool flagR=false, flagC=false;
+        
+        for(int i=0; i<n; i++){
+             if(matrix[i][0]==0)flagR=true;
+        }
+        
+        for(int j=0; j<m; j++){
+            if(matrix[0][j]==0)flagC=true;
+        }
+        
+        
+        // if any cell contains zero, we will make the left edge cell and topmost cell zero
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=matrix[0][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        
+        if(flagR){
+            for(int i=0; i<n; i++){
+                matrix[i][0]=0;
+            }
+        }
+        if(flagC){
+            for(int j=0; j<m; j++){
+                matrix[0][j]=0;
+            }
         }
     }
 };
+// TC=O(n*m), SC=O(1)
