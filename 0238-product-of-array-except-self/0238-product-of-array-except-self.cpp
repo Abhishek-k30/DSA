6bd546ed1c1e306c3013abddb5b227c63b6f59e3
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-03-15 15:03:58 +0530

    Time: 16 ms (59.62%), Space: 27.4 MB (34.75%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,66 +1,37 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> ans;
-        int prod=1;
+        int total=1;
         int zero=0;
-        int c=0;
-        int z=0;
-        int n=nums.size();
-//         vector<int> zr(n);
-        
-//         for(int i=0; i<n; i++){
-//             if(nums[i]==0)z++;
-            
-//             zr[i]=z;
-//         }
-        
         for(int i=0; i<nums.size(); i++){
-            if(nums[i]==0){
-                zero++;
-            }
-            else{
-                prod*=nums[i];
-                c=1;
-            }
+            if(nums[i]!=0)total*=nums[i];
+            else zero++;
         }
-        if(zero==nums.size())prod=0;
         
+        vector<int> ans;
+         if(zero==nums.size()){
+             vector<int> ans2(nums.size() , 0);
+            return ans2;
+        }
         
-        // If number of zeros will be greater then 0 then index having zero will give total Product           // otherwise it will give zero product; 
-        if(zero>1){
-            for(int i=0; i<nums.size(); i++){
-                if(nums[i]==0){
-                    if(zero>=1){
-                        ans.push_back(0);
-                        zero--;  
-                    }
-                    else ans.push_back(prod);
-                }
-                else{
-                    ans.push_back(0);
-                }
+        if(zero==1){
+             for(int i=0; i<nums.size(); i++){
+                if(nums[i]==0)ans.push_back(total);
+                else ans.push_back(0);
             }
         }
-        else if(zero==1){
-            for(int i=0; i<nums.size(); i++){
-                if(nums[i]==0){
-                     
-                     
-                     ans.push_back(prod);
-                }
-                else{
-                    ans.push_back(0);
-                }
-            }
+        else if(zero>1 && zero<nums.size()){
+            vector<int> ans2(nums.size() , 0);
+            return ans2;
         }
+        
         else{
-            for(int i=0; i< nums.size(); i++){
-                ans.push_back(prod/nums[i]);
+            for(int i=0; i<nums.size(); i++){
+                ans.push_back(total/nums[i]);
             }
         }
+        
         return ans;
+        
     }
 };
-
-// TC=O(N), SC=O(N)
