1471c3f7ad2560d839a3d15f38cd23a9b698e9e8
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-03-29 23:23:18 +0530

    Time: 149 ms (38.79%), Space: 93.9 MB (31.00%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -2,22 +2,36 @@ class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
         int n=nums.size();
-        int l=0, ans=0, count=0;
-        for(int r=0; r<n; r++){
-            if(nums[r]%2==1){
-                k--;
-                count=0;
-            }
-            while(k==0){
-                k+=nums[l]%2;
-                count++;
-                l++;
+//         int l=0, ans=0, count=0;
+//         for(int r=0; r<n; r++){
+//             if(nums[r]%2==1){
+//                 k--;
+//                 count=0;
+//             }
+//             while(k==0){
+//                 k+=nums[l]%2;
+//                 count++;
+//                 l++;
                 
+//             }
+//             cout<<count<<" ";
+//             ans+= count;
+//         }
+//         cout<<endl;
+        
+        //Method 2 converting all the nums as 0 or 1. For odd nums[idx]=1 else 0. And then finding the number of subarray sum==k
+        
+        
+        unordered_map<int, int> m;
+        int sum=0, ans=0;
+        m[0]=1;
+        for(int i=0; i<n; i++){
+            sum+=nums[i]%2;
+            if(m.find(sum-k)!=m.end()){
+                ans+=m[sum-k];
             }
-            cout<<count<<" ";
-            ans+= count;
+            m[sum]++;
         }
-        cout<<endl;
         
         return ans;
         
