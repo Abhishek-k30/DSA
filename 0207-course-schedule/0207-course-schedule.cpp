a2702e0aa7b69ffe01f725409498660903fee966
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-05-21 14:47:37 +0530

    Time: 13 ms (79.21%), Space: 16.8 MB (80.27%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,41 +1,43 @@
 class Solution {
+private:
+    bool dfs(int node, vector<int> adj[], vector<int>& vis, vector<int> & pathVis ){
+        vis[node] = 1;
+        pathVis[node] = 1;
+        
+        for(auto it :  adj[node]){
+            if(!vis[it]){
+                if(dfs(it , adj, vis, pathVis))
+                    return true;
+            }
+            else if(pathVis[it])
+                return true;
+        }
+        
+        pathVis[node] = 0;
+        return false;
+    }
 public:
-    vector<int> topoSort(int v, vector<vector<int>>& pre){
-        vector<int> indegree(v , 0);
-        vector<int> adj[v];
-        for(int i=0 ; i< pre.size() ; i++){
+    bool canFinish(int numCourses, vector<vector<int>>& pre) {
+        int V = numCourses ;
+        vector<int> vis(V , 0);
+        vector<int> pathVis(V , 0);
+        
+        // adjancy list
+        vector<int> adj[V];
+         for(int i=0 ; i< pre.size() ; i++){
              int a=pre[i][0];
              int b=pre[i][1];
              adj[a].push_back(b);
-             indegree[b]++;
+              
         }
-         
-       
-        queue<int> q;
-        for(int i=0 ; i<v; i++){
-            if(indegree[i]==0){
-                q.push(i);
+        
+        for(int i=0; i<V; i++){
+            if(!vis[i]){
+                if(dfs(i, adj, vis, pathVis))
+                    return false;
             }
         }
-        vector<int> topo;
-        while(!q.empty()){
-            int node= q.front();
-            q.pop();
-            topo.push_back(node);
-            for(auto it: adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.push(it);
-                }
-            }
-        }
-        return topo;
-    }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> topo=topoSort(numCourses ,prerequisites );
-        if(topo.size()==numCourses){
-            return true;
-        }
-        return false;
+        
+        return true;
     }
 };
