4ae07783ff7f555aa01b762dd0d56682469f646f
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-05-22 08:33:27 +0530

    Time: 46 ms (60.80%), Space: 24.9 MB (47.22%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        if(grid.empty() || grid[0][0]!=0) return -1;
-        int steps=0;
-        queue<pair<int, int>> q;
-        vector<int> dir{0,1,0,-1,0,1,1,-1,-1,1};
-        if(grid[0][0]==0 && m==1 && n==1) return 1;
-        q.push({0,0});
-        grid[0][0]=1;
+        int n = grid.size();
+        
+        if(grid[0][0]!=0 || grid[n-1][n-1]!=0)return -1;
+        vector<vector<int>> dist(n, vector<int> (n, 1e9));
+        
+        //bfs
+        queue<pair<int, pair<int, int>>> q;         //{dist,{row, col}}
+        q.push({1,{0,0}});
+        dist[0][0] = 1;
+        
         while(!q.empty()){
-            steps++;
-            int size=q.size();
-            for(int i=0; i<size; i++){
-                 auto cord=q.front();
-                 q.pop();
-                for(int j=0; j<9; j++){
-                    int nrow=cord.first+dir[j];
-                    int ncol=cord.second+dir[j+1];
+            int dis = q.front().first;
+            int row = q.front().second.first;
+            int col = q.front().second.second;
+            q.pop();
+            
+            //traverse in 8 directions
+            for(int i=-1; i<=1; i++){
+                for(int j=-1; j<=1; j++){
                     
-                    if(nrow<n && nrow>=0 && ncol<m && ncol>=0 && grid[nrow][ncol]!=1){
-                        if(nrow==n-1 && ncol==m-1) return steps+1;
-                        else{
-                            q.push({nrow, ncol});
-                            grid[nrow][ncol]=1;
-                        }
+                    int nrow = row + i;
+                    int ncol = col + j;
+                    
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<n 
+                      && grid[nrow][ncol]==0 && dis + 1 < dist[nrow][ncol]){
+                        dist[nrow][ncol] = 1 + dis;
+                        if(nrow==n-1 && ncol == n-1)
+                            return  dis + 1 ;
+                        q.push({dis + 1 , {nrow, ncol}});
                     }
                 }
             }
-            
         }
         
-        return -1;
-        
+        return  (dist[n-1][n-1]==1e9? -1 : dist[n-1][n-1]);
     }
 };
-//  TC=O(N+M) , SC=O(N+M);
