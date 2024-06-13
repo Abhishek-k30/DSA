a2a87bdf91d81d2be35b04318d8b16f0363b4fea
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-06-13 18:45:56 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (8.30%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,41 +1,25 @@
 class Solution {
+private:
+    int f(int i , int j , vector<vector<int>> &obstacleGrid, vector<vector<int>> &dp){
+        if(i>=0 && j>=0  && obstacleGrid[i][j]==1)
+            return 0;
+        if(i<0 || j<0)
+            return 0;
+        if(i==0 && j==0)
+            return 1;
+        
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        return dp[i][j] = f(i-1, j, obstacleGrid, dp) + f(i, j-1, obstacleGrid, dp);
+    } 
 public:
-    int f(int i, int j , vector<vector<int>>& Grid, vector<vector<int>> &dp){
-        int n=Grid.size();
-        int m=Grid[0].size();
-        
-        // Base Case
-        if(i<0 ||i>=n || j<0 || j>=m)return 0;
-        if(Grid[i][j]==1)return 0;
-        if(i==n-1 && j==m-1) return 1;
-            
-            
-        
-        
-        
-        
-        // Memoization
-        if(dp[i][j]!=-1)return dp[i][j];
-        
-        
-        // Right movement
-        int right=f(i, j+1, Grid, dp);
-        
-        // Down movement
-        int down=f(i+1,j, Grid, dp);
-        
-        
-        // total sum of movement
-        return dp[i][j]=right+down;
-        
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int n=obstacleGrid.size();
-        int m=obstacleGrid[0].size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
         
-        return f(0,0,obstacleGrid, dp);
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        
+        return f(n-1, m-1, obstacleGrid, dp);
     }
 };
-
-// TC O(N*M) SC O(N*M + max(N,M))
