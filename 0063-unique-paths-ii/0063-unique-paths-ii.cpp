fd31e1abf2aa5fb3518a923978fb8d8425b8fe69
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-06-13 18:50:16 +0530

    Time: 3 ms (53.48%), Space: 10.3 MB (8.30%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -18,8 +18,26 @@ public:
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
         
-        vector<vector<int>> dp(n, vector<int>(m, -1));
+        vector<vector<int>> dp(n, vector<int>(m, 0));
         
-        return f(n-1, m-1, obstacleGrid, dp);
+        //return f(n-1, m-1, obstacleGrid, dp);
+        
+        for(int i=0; i<n ; i++){
+            for(int j=0; j<m; j++){
+                if(obstacleGrid[i][j]==1)
+                    dp[i][j] = 0;
+                else if(i==0 && j==0)
+                    dp[i][j] = 1;
+                else{
+                    int down = 0, right = 0;
+                    if(i>0)down = dp[i-1][j];
+                    if(j>0)right = dp[i][j-1];
+                    
+                    dp[i][j] = down + right;
+                }
+            }
+        }
+        
+        return dp[n-1][m-1];
     }
 };
