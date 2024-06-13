752383afe8cf99a48276281d04c9e9f4790201c0
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-06-13 16:15:05 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (49.23%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -24,12 +24,24 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1)return nums[0];
+        if(n==2)return max(nums[0], nums[1]);
         vector<int> t1(nums.begin()+1,nums.end()), t2(nums.begin(), nums.end()-1);
         
-        vector<int> dp(n+1, -1), dp1(n+1,-1);
+//         vector<int> dp(n+1, -1), dp1(n+1,-1);
          
         
-        return max(f(0,t1, dp), f(0,t2, dp1));
+//         return max(f(0,t1, dp), f(0,t2, dp1));
+        
+        t1[1] = max(t1[0], t1[1]);
+        t2[1] = max(t2[0], t2[1]);
+        
+        for(int i=2; i<n-1; i++){
+            t1[i] = max(t1[i-1], t1[i] + t1[i-2]);
+            t2[i] = max(t2[i-1], t2[i] + t2[i-2]);
+            
+        }
+        
+        return max(t1.back(), t2.back());
     }
 };
 
