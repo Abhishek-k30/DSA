543195712f50f8b912ad70a93027c2e1a4700bd5
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-06-17 08:48:53 +0530

    Time: 57 ms (15.93%), Space: 51.9 MB (8.81%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,35 +1,29 @@
 class Solution {
-public:
-    int f(int ind, int tar, vector<int>& coins , vector<vector<int>> &dp ){
-        int n=coins.size();
-        
-        // Base Case
-        if(ind<0 || ind>=n)return 1e9;
-        if(ind==n-1){
-            if(tar% coins[ind]==0)return tar/coins[ind];
-            return 1e9;
+private:
+    int f(int ind, int sum, vector<int> &coins, vector<vector<int>> &dp ){
+        if(ind<0){
+            if(sum>0)
+                return 1e9;
+            else return 0;
         }
         
-        // Memoization
-        if(dp[ind][tar]!=-1)return dp[ind][tar];
+        if(dp[ind][sum]!=-1)
+            return dp[ind][sum];
         
-        // Not Pick
-        int notPick= f(ind+1, tar,coins , dp);
+        int notTake = f(ind-1, sum, coins, dp);
+        int take = INT_MAX;
+        if(coins[ind]<= sum)
+            take = 1 + f(ind, sum - coins[ind] , coins, dp);
         
-        // Pick
-        int Pick=INT_MAX;
-        if(tar-coins[ind]>=0){
-            Pick=1+f(ind, tar-coins[ind], coins, dp);
-        }
-        
-        // Returning Minimum
-        return dp[ind][tar]=min(Pick, notPick);
+        return dp[ind][sum] = min(take, notTake);
     }
+public:
     int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        vector<vector<int>> dp(n+1, vector<int>(amount+1,-1));
-        int ans=f(0, amount, coins , dp);
-        if(ans>=1e9)return -1;
+        int n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
+        int ans =  f(n-1, amount, coins, dp);
+        if(ans>=1e9)
+            return -1;
         return ans;
     }
 };
