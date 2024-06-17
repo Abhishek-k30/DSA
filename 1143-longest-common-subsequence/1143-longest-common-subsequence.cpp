ec05ff2461b4c4e367dc23db720d657fbb1af58e
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-06-17 16:35:36 +0530

    Time: 44 ms (14.74%), Space: 25.7 MB (32.28%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,40 +1,22 @@
 class Solution {
-public:
-    int f(int index1, int index2, string &s, string &t, vector<vector<int>> &dp){
+private:
+    int f(int i , int j , string &text1 , string &text2, vector<vector<int>> &dp){
+        if(i<0 || j<0)
+            return 0;
         
-        // Base Case
-        if(index1<0 || index2<0)return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
         
-        // Memoization
-        if(dp[index1][index2]!=-1) return dp[index1][index2];
-        
-        //Match
-        if(s[index1]==t[index2]) return 1+f(index1-1, index2-1, s, t, dp);
-        
-        // Not Match
-        return dp[index1][index2]=max(f(index1-1, index2, s, t, dp), f(index1, index2-1, s, t, dp));
+        if(text1[i]==text2[j])
+            return dp[i][j] = 1+f(i-1, j-1, text1, text2, dp);
+        else return dp[i][j] = max(f(i-1, j, text1, text2, dp), f(i, j-1, text1, text2, dp));
     }
+public:
     int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.size();
-        int m=text2.size();
+        int n1 = text1.size();
+        int n2 = text2.size();
         
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-        //return f(n-1, m-1, text1, text2, dp);
-        // Tabulation
-        
-        
-         for(int i=1; i<=n; i++){
-             for(int j=1; j<=m; j++){
-                 
-                 if(text1[i-1]==text2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                 else dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
-             }
-         }
-        
-        return dp[n][m];
-        
-            
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        return f(n1-1, n2-1, text1, text2, dp);
     }
 };
-
-// TC=O(N*M), SC=O(N*M)
