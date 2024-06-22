37efa1fb09e5530f10b3d86720a602bdc178f326
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-06-22 08:18:23 +0530

    Time: 95 ms (59.42%), Space: 72.1 MB (29.33%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int n=nums.size();
-//         int l=0, ans=0, count=0;
-//         for(int r=0; r<n; r++){
-//             if(nums[r]%2==1){
-//                 k--;
-//                 count=0;
-//             }
-//             while(k==0){
-//                 k+=nums[l]%2;
-//                 count++;
-//                 l++;
+        int ans = 0;
+        deque<int> dq;
+        dq.push_back(-1);
+        
+        for(int i = 0; i< nums.size(); i++){
+            if(nums[i]%2)
+                dq.push_back(i);
+            if(dq.size()> k+1)
+                dq.pop_front();
+            if(dq.size()==k+1){
+                ans+= dq[1] - dq[0];
                 
-//             }
-//             cout<<count<<" ";
-//             ans+= count;
-//         }
-//         cout<<endl;
-        
-        //Method 2 converting all the nums as 0 or 1. For odd nums[idx]=1 else 0. And then finding the number of subarray sum==k
-        
-        
-        unordered_map<int, int> m;
-        int sum=0, ans=0;
-        m[0]=1;
-        for(int i=0; i<n; i++){
-            sum+=nums[i]%2;
-            if(m.find(sum-k)!=m.end()){
-                ans+=m[sum-k];
+                //cout<<dq[1]<<" "<<dq[0]<<endl;
             }
-            m[sum]++;
         }
         
+        //cout<<endl;
         return ans;
-        
-        
     }
 };
