6b4e8121ebcfbe97f84622a631e657b8c720d5fe
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-06-23 11:57:45 +0530

    Time: 150 ms (17.55%), Space: 81.1 MB (17.55%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
-        int n=nums.size();
-        int l=0, ans=0;
+        int n = nums.size();
+        int maxSize = 0;
         multiset<int> st;
-        for(int r=0; r<n; r++){
-             st.insert(nums[r]);
+        int l = 0;
+        
+        for(int r = 0; r<n; r++){
+            st.insert(nums[r]);
             
-            
-             while(l<n && abs(*st.rbegin()-*st.begin())>limit){
-                 // int cnt=st.count(nums[l]);
-                 st.erase(st.find(nums[l]));
-                 l++;
-                  
+            while(l<=r && abs(*st.begin() - *(prev(st.end()))) > limit){
+                st.erase(st.find(nums[l]));
+                l++;
             }
             
-            
-             
-           
-            ans=max(ans, r-l+1);
-            
-//             cout<<l+1<<" "<<r+1<<" "<<ans<<endl;
-            
+            maxSize = max(maxSize, r-l+1);
         }
         
-       
-        
-        return ans;
+        return maxSize;
     }
 };
