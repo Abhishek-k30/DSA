901f9ee77a34398e12d792eb4c312d8f803bbff8
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-07-21 09:04:29 +0530

    Time: 7 ms (83.21%), Space: 16.7 MB (28.91%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
+        int n = matrix.size();
+        int m = matrix[0].size();
         
-        // flags for tracking zero of first row and column
-        bool flagR=false, flagC=false;
+        bool firstRowContainsZero = false;
+        bool firstColContainsZero = false;
         
-        for(int i=0; i<n; i++){
-             if(matrix[i][0]==0)flagR=true;
+        for(int i = 0; i<n; i++){
+            if(matrix[i][0]==0)
+                firstColContainsZero= true;
         }
         
-        for(int j=0; j<m; j++){
-            if(matrix[0][j]==0)flagC=true;
+        for(int j = 0; j<m; j++){
+            if(matrix[0][j]==0)
+                firstRowContainsZero= true;
         }
         
         
-        // if any cell contains zero, we will make the left edge cell and topmost cell zero
-        for(int i=1; i<n; i++){
-            for(int j=1; j<m; j++){
-                if(matrix[i][j]==0){
-                    matrix[i][0]=matrix[0][j]=0;
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<m; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
         
-        for(int i=1; i<n; i++){
-            for(int j=1; j<m; j++){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
-                    matrix[i][j]=0;
-                }
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<m; j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                    matrix[i][j] = 0;
             }
         }
         
-        if(flagR){
-            for(int i=0; i<n; i++){
-                matrix[i][0]=0;
-            }
+        if(firstColContainsZero){
+            for(int i = 0; i<n; i++)
+                matrix[i][0] = 0;
         }
-        if(flagC){
-            for(int j=0; j<m; j++){
-                matrix[0][j]=0;
-            }
+        
+        if(firstRowContainsZero){
+            for(int j = 0; j<m; j++)
+                matrix[0][j] = 0;
         }
     }
 };
-// TC=O(n*m), SC=O(1)
