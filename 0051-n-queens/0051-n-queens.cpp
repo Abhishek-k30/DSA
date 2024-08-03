70e3189531ff90b97ea6d8627d608e08efec28fb
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-08-03 18:15:50 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (60.01%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -49,6 +49,27 @@ public:
             }
         }
     }
+    void solve(int col, int n, vector<string> &board, vector<vector<string>> &ans,
+              vector<int> &left, vector<int> &upperDiag, vector<int> & lowerDiag){
+        if(col == n){
+            ans.push_back(board);
+            return;
+        }
+        
+        for(int row = 0; row<n; row++){
+            if(left[row] == 0 && lowerDiag[row+col] == 0 && upperDiag[n-1 + col-row] == 0){
+                board[row][col] = 'Q';
+                left[row] = 1;
+                lowerDiag[row+col] = 1;
+                upperDiag[n-1+col-row] = 1;
+                solve(col+1, n, board, ans, left, upperDiag, lowerDiag);
+                board[row][col] = '.';
+                left[row] = 0;
+                lowerDiag[row+col] = 0;
+                upperDiag[n-1 + col-row] = 0;
+            }
+        }
+    }
 public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
@@ -59,9 +80,15 @@ public:
             board[i] = s;
         }
         
-        backtrack(0, n, board, ans);
+        //backtrack(0, n, board, ans);
+        
+        vector<int> left(n, 0), upperdiagonal(2*n-1, 0), lowerdiagonal(2*n-1, 0);
+        solve(0, n, board, ans, left, upperdiagonal, lowerdiagonal);
         return ans;
         
         
     }
 };
+
+//TC = O(n!)
+//SC = O(n*n)
