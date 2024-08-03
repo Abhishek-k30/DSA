3d3173106bd14bdf465dd029f75816c03c0865e1
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-08-03 17:55:53 +0530

    Time: 3 ms (69.20%), Space: 9 MB (28.02%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,66 @@
+class Solution {
+private:
+    bool isSafe(int row, int col, int n, vector<string> &board){
+        // left upper diagonal
+        
+        int curRow = row, curCol = col;
+        while(curRow >=0 && curCol >=0){
+            if(board[curRow][curCol]=='Q')
+                return false;
+            
+            curRow--;
+            curCol--;
+        }
+        
+        // left
+        curRow = row, curCol = col;
+        while(curCol>=0){
+            if(board[curRow][curCol]=='Q')
+                return false;
+            
+            curCol--;
+        }
+        
+        // left down diagonal
+        curRow = row, curCol = col;
+         while(curRow <n && curCol >=0){
+            if(board[curRow][curCol]=='Q')
+                return false;
+            
+            curRow++;
+            curCol--;
+        }
+        
+        return true;
+        
+    }
+public:
+    void backtrack(int col, int n,  vector<string> &board, set<vector<string>> &ans){
+        if(col == n){
+            ans.insert(board);
+            return;
+        }
+        
+        for(int row = 0; row<n; row++){
+            if(isSafe(row, col, n,  board)){
+                board[row][col] = 'Q';
+                backtrack(col+1, n, board, ans);
+                board[row][col] = '.';
+            }
+        }
+    }
+public:
+    int totalNQueens(int n) {
+         set<vector<string>> ans;
+        vector<string> board(n);
+        string s(n, '.');
+        
+        for(int i = 0; i<n; i++){
+            board[i] = s;
+        }
+        
+        backtrack(0, n, board, ans);
+        return ans.size();
+        
+    }
+};
