9f768bc83211e695b97bed31aba8163e30e32ca6
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-08-05 12:03:43 +0530

    Time: 94 ms (56.62%), Space: 61.1 MB (46.20%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,31 +1,31 @@
 class Solution {
-public:
-    bool check(string &s, int start, int end){
-        while(start<=end){
-            if(s[start++]!=s[end--])return false;
+private:
+    bool isPalindrome(string s, int start, int end){
+        while(start<end){
+            if(s[start++]!=s[end--])
+                return false;
         }
         return true;
     }
-    void dfs(int index, string &s, vector<string> &temp, vector<vector<string>> &ans){
-        if(index==s.size()){
-            ans.push_back(temp);
+public:
+    void func(int ind, string s, vector<string> &path, vector<vector<string>> &ans){
+        if(ind == s.size()){
+            ans.push_back(path);
             return;
         }
-
-        for(int i=index; i<s.size(); i++){
-            if(check(s, index, i)){
-                temp.push_back(s.substr(index, i-index+1));
-                dfs(i+1, s, temp, ans);
-                temp.pop_back();
+        
+        for(int i = ind; i<s.size(); i++){
+            if(isPalindrome(s, ind, i)){
+                path.push_back(s.substr(ind, i-ind+1));
+                func(i+1, s, path, ans);
+                path.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
-       vector<vector<string>> ans;
-       vector<string> temp;
-       dfs(0, s, temp, ans);
-
-       return ans;
-
+        vector<vector<string>> ans;
+        vector<string> path;
+        func(0, s, path, ans);
+        return ans;
     }
 };
