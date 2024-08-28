8018def63c6d97e7be3f20216a8180d69c52f29c
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-08-28 10:27:23 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (11.27%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,48 +1,28 @@
 class Solution {
 public:
-    int f(int ind, vector<int> nums, vector<int> &dp){
-        int n=nums.size();
-        
-        // Base Case
-         
-        if(ind<0 || ind>=n)return 0;
-        if(ind==n-1)return nums[ind];
-        
-        
-        // Memoiation
-        if(dp[ind]!=-1)return dp[ind];
-        
-        // Not Pick
-        int notPick=f(ind+1,nums, dp);
-        
-        // Pick
-        int Pick=nums[ind]+f(ind+2, nums, dp);
-        
-        // Return Maximum
-        return dp[ind]=max(Pick, notPick);
-    }
     int rob(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
         if(n==1)return nums[0];
-        if(n==2)return max(nums[0], nums[1]);
-        vector<int> t1(nums.begin()+1,nums.end()), t2(nums.begin(), nums.end()-1);
+        vector<int> temp1(nums.begin()+1, nums.end());
+        vector<int> temp2(nums.begin(), nums.end()-1);
         
-//         vector<int> dp(n+1, -1), dp1(n+1,-1);
-         
-        
-//         return max(f(0,t1, dp), f(0,t2, dp1));
-        
-        t1[1] = max(t1[0], t1[1]);
-        t2[1] = max(t2[0], t2[1]);
-        
-        for(int i=2; i<n-1; i++){
-            t1[i] = max(t1[i-1], t1[i] + t1[i-2]);
-            t2[i] = max(t2[i-1], t2[i] + t2[i-2]);
+        vector<int> dp1(n, 0), dp2(n, 0);
+        for(int i = 0; i<n-1; i++){
+            int lastPrev = 0 , prev = 0;
+            if(i-2>=0)lastPrev = dp1[i-2];
+            if(i-1>=0)prev = dp1[i-1];
             
+            dp1[i] = max(temp1[i]+lastPrev, prev);
+        }
+        for(int i = 0; i<n-1; i++){
+            int lastPrev = 0 , prev = 0;
+            if(i-2>=0)lastPrev = dp2[i-2];
+            if(i-1>=0)prev = dp2[i-1];
+            
+            dp2[i] = max(temp2[i]+lastPrev, prev);
         }
         
-        return max(t1.back(), t2.back());
+        return max(dp1[n-2], dp2[n-2]);
+            
     }
 };
-
-//TC=O(N), SC=O(N)*4+O(N)
