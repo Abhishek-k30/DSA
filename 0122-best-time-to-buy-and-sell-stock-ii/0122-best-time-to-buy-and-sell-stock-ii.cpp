b05dab58968de7cb26b49be0e9344abb6b44751e
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-09-05 16:42:40 +0530

    Time: 14 ms (7.07%), Space: 20.5 MB (15.91%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,33 +1,31 @@
 class Solution {
 private:
-    int f(int ind, int buy, vector<int> &prices, vector<vector<int>> &dp){
-        if(ind==prices.size())
+    int solve(int ind, int buy, vector<int>&prices, vector<vector<int>>&dp){
+        if(ind == prices.size())
             return 0;
         if(dp[ind][buy]!=-1)
             return dp[ind][buy];
         
+         int profit = 0;
+        
+        // if buy == 1 means we need to buy the stocks
+        // else if buy == 0 means we have already buyed stock, we now need to sell it
+        
         if(buy){
-            return dp[ind][buy] = max(-prices[ind] + f(ind+1, 0, prices, dp), f(ind+1, 1, prices, dp));
+            profit = max(-prices[ind] + solve(ind+1,0,prices, dp) ,
+                        solve(ind+1,1, prices, dp));
         }
-        else return dp[ind][buy] = max(prices[ind] + f(ind+1, 1, prices, dp), f(ind+1, 0, prices, dp));
+        else {
+            profit = max(prices[ind] + solve(ind+1,1,prices,dp) ,
+                         solve(ind+1,0,prices,dp));
+        }
+        
+        return dp[ind][buy] = profit;
     }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        
-        vector<vector<int>> dp(n+1, vector<int>(2, 0));
-        
-        //return f(0, 1, prices, dp);
-        
-        for(int i = n-1; i>=0; i--){
-            for(int j = 0; j<=1; j++){
-                if(j==1){
-                    dp[i][j] = max(-prices[i] + dp[i+1][0], dp[i+1][1]);
-                }
-                else dp[i][j] = max(prices[i] + dp[i+1][1] , dp[i+1][0]);
-            }
-        }
-        
-        return dp[0][1];
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return solve(0, 1, prices, dp);
     }
 };
