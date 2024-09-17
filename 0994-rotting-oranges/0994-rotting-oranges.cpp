0925f09d148ff15bb5150573088b21b7982094dc
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-09-17 10:57:20 +0530

    Time: 0 ms (100.00%), Space: 16.5 MB (39.77%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,50 +1,55 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        queue<pair<pair<int, int>, int>> q; // {{row, col}, time}
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j]==2){
-                    vis[i][j]=1;
-                    q.push({{i, j}, 0});
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<int>> vis(n, vector<int>(m));
+        
+        queue<pair<pair<int, int>, int>> q;     //{{row, col}, time}
+        
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(grid[i][j] == 2){
+                    vis[i][j] = 1;
+                    q.push({{i,j},0});
                 }
             }
         }
         
-        int mxTime=0;
-        // bfs
-        int dr[]={-1,0,1,0};
-        int dc[]={0,1,0,-1};
+        int dr[] = {0,-1,0,1};
+        int dc[] = {1,0,-1,0};
+        int maxTime = 0;
+        
         while(!q.empty()){
-            int row=q.front().first.first;
-            int col= q.front().first.second;
-            int time= q.front().second;
-            mxTime=max(mxTime, time);
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int time = q.front().second;
+            
             q.pop();
             
-            for(int k=0; k<4; k++){
-                int nrow= row+dr[k];
-                int ncol= col+dc[k];
+            maxTime = max(maxTime, time);
+            
+            for(int i = 0; i<4; i++){
+                int nrow = row + dr[i];
+                int ncol = col + dc[i];
                 
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&
-                  vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                    vis[nrow][ncol]=1;
-                    q.push({{nrow , ncol}, time+1});
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m
+                  && grid[nrow][ncol]== 1 && !vis[nrow][ncol] ){
+                    vis[nrow][ncol] = 1;
+                    q.push({{nrow, ncol}, time+1});
                 }
             }
-            
         }
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(!vis[i][j] && grid[i][j])
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(!vis[i][j] && grid[i][j] == 1){
                     return -1;
+                }
             }
         }
         
-        return mxTime;
+        return maxTime;
     }
 };
