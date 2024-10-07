1e3bf067c85f2198d661a8e8eb9555fe87fec1e5
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-10-07 11:16:47 +0530

    Time: 3 ms (60.58%), Space: 12.4 MB (44.35%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -35,7 +35,32 @@ public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         
         int n = days.size();
-        vector<int> dp(n+1, -1);
-        return solve(0, days, costs, dp);
+        // vector<int> dp(n+1, -1);
+        // return solve(0, days, costs, dp);
+        
+        int maxi = days.back();
+        vector<int> dp(maxi+1,0);
+        
+        set<int> st(days.begin(), days.end());
+        
+        for(int i = 1; i<=maxi; i++){
+            if(st.find(i)==st.end()){
+                dp[i] = dp[i-1];
+                continue;
+            }
+            
+            int one = 1e9, week = 1e9, month = 1e9;
+            
+            if(i-1>=0)one = costs[0] + dp[i-1];
+            else one = costs[0];
+            if(i-7>=0)week = costs[1] + dp[i-7];
+            else week = costs[1];
+            if(i-30>=0)month = costs[2] + dp[i-30];
+            else month = costs[2];
+            
+            dp[i] = min({  one, week, month});
+        }
+        
+        return dp[maxi];
     }
 };
