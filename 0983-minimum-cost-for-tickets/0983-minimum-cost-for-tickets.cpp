2fdd5d1424d48fa6188b5d5b1f5ca7506a72fa36
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-10-07 11:01:58 +0530

    Time: 3 ms (60.58%), Space: 12.9 MB (26.91%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,28 +1,41 @@
 class Solution {
+private:
+    int index(int cur_idx, int dur, vector<int>&days){
+        int val = days[cur_idx] + dur-1;
+        
+        int l = cur_idx, r = days.size()-1;
+        int ans = days.size();
+        
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(days[mid]>val){
+                ans = mid;
+                r = mid-1;
+            }
+            else l = mid+1;
+        }
+        
+        return ans;
+    }
 public:
-    vector<int> dp;
-    int sub(vector<int> &day, vector<int> &cost, int si)    // subproblem
-    {
-        int n = day.size();
-        if(si>=n)   return 0;
+    int solve(int idx, vector<int>&days, vector<int>&costs, vector<int>&dp){
+        if(idx>=days.size()){
+            return 0;
+        }
         
-        if(dp[si])  return dp[si];      // ie we aready knows the answer so no need to re calculate , return answer.
+        if(dp[idx]!=-1)return dp[idx];
+        int oneDay = 1e9, weekDay = 1e9, monthDay = 1e9;
         
-        int cost_day = cost[0] + sub(day , cost , si+1);
+        oneDay = costs[0] + solve(idx+1, days, costs, dp);
+        weekDay = costs[1] + solve(index(idx, 7, days), days, costs, dp);
+        monthDay = costs[2] + solve(index(idx, 30, days), days, costs, dp);
         
-        int i;
-        for(i = si ; i<n and day[i]<day[si]+7 ; i++);
-        int cost_week = cost[1] + sub(day, cost, i);
+        return dp[idx] = min({oneDay, weekDay, monthDay});
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
         
-        for(i = si ; i<n and day[i]<day[si]+30 ; i++);
-        int cost_month = cost[2] + sub(day, cost, i);     
-        
-        dp[si] = min({cost_day, cost_week , cost_month  });   // we store answer for future references 
-        
-        return dp[si];
-    } 
-    int mincostTickets(vector<int>& days, vector<int>& costs) {        
-        dp.resize(367);
-        return sub(days,costs,0);    
+        int n = days.size();
+        vector<int> dp(n+1, -1);
+        return solve(0, days, costs, dp);
     }
 };
