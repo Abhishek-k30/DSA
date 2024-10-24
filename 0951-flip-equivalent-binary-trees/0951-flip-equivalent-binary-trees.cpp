dcc59dfafe7570620fd88e7eab83bdfcc6b02f14
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-10-24 19:23:26 +0530

    Time: 0 ms (100.00%), Space: 14.5 MB (61.52%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
@@ -10,42 +10,15 @@
  * };
  */
 class Solution {
-private:
-    void solve(TreeNode* root, vector<vector<int>> &v){
-        if(!root){
-            return;
-        }
-        solve(root->left, v);
-        if(root->left)v[root->val][0] = root->left->val;
-        if(root->right)v[root->val][1] = root->right->val;
-        solve(root->right, v);
-    }
 public:
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        if(!root1 || !root2 ){
-            if(!root1 && !root2)return true;
-            return false;
+        if(!root1 && !root2){
+            return true;
         }
-        vector<vector<int>> v1(102, vector<int>(2,-1)), v2(102,vector<int>(2, -1));
+        if(!root1 || !root2)return false;
         
-        solve(root1, v1);
-        solve(root2, v2);
-        v1[0][0] = root1->val;
-        v2[0][0] = root2->val;
-        v1[0][1] = root1->val;
-        v2[0][1] = root2->val;
-        for(int i = 1; i<=100; i++){
-            vector<int> t1 = v1[i];
-            vector<int> t2 = v2[i];
-            
-            sort(t1.begin(), t1.end());
-            sort(t2.begin(), t2.end());
-            
-            if(t1!=t2){
-                return false;
-            }
-        }
+        if(root1->val != root2->val)return false;
         
-        return true;
+        return ((flipEquiv(root1->left, root2->left) || flipEquiv(root1->left , root2->right)) && (flipEquiv(root1->right, root2->left) || flipEquiv(root1->right, root2->right)));
     }
 };
