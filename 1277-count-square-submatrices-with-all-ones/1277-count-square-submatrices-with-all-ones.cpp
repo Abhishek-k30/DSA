37fceff741d53811e409324691434f621f1a29f5
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-10-27 19:38:35 +0530

    Time: 7 ms (51.85%), Space: 29.4 MB (5.99%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -22,18 +22,39 @@ public:
          n = matrix.size();
          m = matrix[0].size();
         
-         memset(dp, -1, sizeof(dp));
-         int totalSquare = 0;
+//          memset(dp, -1, sizeof(dp));
+//          int totalSquare = 0;
         
-         for(int i = 0; i<n; i++){
-             for(int j = 0; j<m; j++){
-                 if(matrix[i][j]){
-                     totalSquare+= solve(i, j, matrix);
-                 }
-             }
-         }
+//          for(int i = 0; i<n; i++){
+//              for(int j = 0; j<m; j++){
+//                  if(matrix[i][j]){
+//                      totalSquare+= solve(i, j, matrix);
+//                  }
+//              }
+//          }
+        
+//         return totalSquare;
+        
+        // Top down
+        
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        
+        int totalSquare = 0;
+        for(int i = n-1; i>=0; i--){
+            for(int j = m-1; j>=0; j--){
+                if(matrix[i][j] == 1){
+                    int right = dp[i][j+1];
+                    int dg = dp[i+1][j+1];
+                    int down = dp[i+1][j];
+                    
+                    dp[i][j] = 1 + min({right, dg, down});
+                }
+                else dp[i][j]= 0;
+                
+                totalSquare+=dp[i][j];
+            }
+        }
         
         return totalSquare;
-        
     }
 };
