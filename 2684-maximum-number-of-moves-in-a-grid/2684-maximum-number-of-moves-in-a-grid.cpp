6466714c12d372435dd0b0d0bf5cbf30e079c758
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-10-29 18:49:04 +0530

    Time: 25 ms (50.00%), Space: 74.6 MB (25.57%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    int n , m, maxCnt = 0;
+    int n , m;
     int dr[3] = {-1,0,1};
-    int dc[3] = {1,1,1};
-    void dfs(int row, int col, int cnt, vector<vector<int>> &vis, vector<vector<int>>& grid){
+    int dfs(int row, int col, vector<vector<int>> &grid, vector<vector<int>>&memo){
         
-        vis[row][col] = 1;
-        
-        maxCnt = max(maxCnt, cnt);
+        if(memo[row][col]!=-1)return memo[row][col];
+        int moves = 0;
         
         for(int k = 0; k<3; k++){
             int nrow = row + dr[k];
-            int ncol = col + dc[k];
+            int ncol = col + 1;
             
             if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&
-              !vis[nrow][ncol] && grid[nrow][ncol] > grid[row][col]){
-                dfs(nrow, ncol, cnt+1, vis, grid);
+              grid[nrow][ncol] > grid[row][col]){
+                moves = max(moves , 1 + dfs(nrow, ncol, grid, memo));
             }
         }
         
+        return memo[row][col] = moves;
     }
     int maxMoves(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
         
-        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>> memo(n, vector<int>(m, -1));
+        int res = 0;
+        
         for(int i = 0; i<n; i++){
             
-            dfs(i,0,0,vis,grid);
+           res = max(res, dfs(i,0,grid, memo));
         }
         
-        return maxCnt;
+        return res;
     }
 };
