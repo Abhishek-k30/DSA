e52b0d6b4c57b77ceb166b99b55b05ce1585c7c8
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-11-11 00:45:27 +0530

    Time: 219 ms (27.47%), Space: 88.4 MB (35.93%) - LeetHub

--- 3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -1,50 +1,39 @@
 class Solution {
 public:
-   void add(vector<int> &bits, int val){
-        int idx=0;
-       while(val>0){
-           bits[idx]+= (val%2);
-           val/=2;
-           idx++;
-       }
-   }
-    void remove(vector<int> &bits, int val){
-        int idx=0;
-       while(val>0){
-           bits[idx]-= (val%2);
-           val/=2;
-           idx++;
-       }
-   }
-    bool good(vector<int> &bits, int k){
-        int cnt=0;
-        for(int i=0; i<=31; i++){
-             cnt+= (bits[i]? 1: 0)*(pow(2, i));
+    void add(int nums,  vector<int> &bits){
+        for(int i = 0; i<32; i++){
+            bits[i]+= ((nums&(1<<i))!=0 ? 1 : 0);
+        }
+    }
+    void remove(int nums , vector<int> &bits){
+        for(int i = 0; i<32; i++){
+            bits[i]-= ((nums&(1<<i))!=0 ? 1 : 0);
+        }
+    }
+    bool good(int k, vector<int> &bits){
+        long long cur = 0;
+        for(int i = 0; i<32; i++){
+            cur+= (long long)pow(2, i)*(bits[i] ? 1 : 0);
         }
         
-        return(cnt>=k);
+        return cur>=k;
     }
     int minimumSubarrayLength(vector<int>& nums, int k) {
-        int n=nums.size();
+        int n = nums.size();
         vector<int> bits(32, 0);
-        int l=0, ans=1e9;
-        for(int r=0; r<n; r++){
-            add(bits, nums[r]);
-            while(l<r && good(bits, k)){
-                
-               if(good(bits, k)) ans=min(ans, r-l+1);
-                remove(bits, nums[l]);
+        int l = 0, len = INT_MAX;
+        
+        for(int r = 0; r<n; r++){
+            add(nums[r], bits);
+            while(l<=r && good(k, bits)){
+                len = min(len, r-l+1);
+                remove(nums[l], bits);
                 l++;
             }
             
-            if(good(bits, k))
-            ans=min(ans, r-l+1);
-            
-            
+             
         }
-         
-        ans=(ans !=1e9 ? ans : -1);
-        return ans;
-        
+        if(len == INT_MAX)len = -1;
+        return len;
     }
 };
