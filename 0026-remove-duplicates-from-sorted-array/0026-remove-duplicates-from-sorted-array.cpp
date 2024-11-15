d23f511505092d7f754de5e979dfdebec9e7de82
Abhishek Kumar <kumarabhishek0776@gmail.com>
2024-11-15 17:39:16 +0530

    Time: 0 ms (100.00%), Space: 21.4 MB (57.46%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -5,12 +5,13 @@ public:
         int prev = -101;
         int k = 0;
         for(int i = 0; i<n; i++){
-            if(nums[i] ==  prev)k++;
+            if(nums[i] == prev)k++;
             else{
                 prev = nums[i];
                 nums[i-k] = nums[i];
             }
         }
+        
         return n - k;
     }
 };
